b55aee15c54199c465a4ee36dca7461e21acab9a
treesnake <info@treesnake.co.uk>
2025-12-29 10:35:02 +0000

    Checker

---
+++ UpdatedDateChecker/UpdatedDateChecker/ModMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace UpdatedDateChecker
+{
+    public class ModMetaData
+    {
+        [JsonPropertyName("modId")]
+        public string ModId { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+    }
+}
--- UpdatedDateChecker/UpdatedDateChecker/Program.cs
+++ UpdatedDateChecker/UpdatedDateChecker/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace UpdatedDateChecker
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace UpdatedDateChecker
 {
     internal class Program
     {
+        public const string target = "./mods.json";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HttpClient httpClient = new HttpClient()
+            { 
+                BaseAddress = new Uri("https://reforger.armaplatform.com/workshop/")
+            }
+            ;
+            Console.WriteLine($"Checking {target}");
+
+            string fileContents = File.ReadAllText(target);
+
+            var modList = JsonSerializer.Deserialize<List<ModMetaData>>(fileContents);
+            Console.WriteLine("Mod Name, Last Modified");
+            foreach(var mod in modList)
+            {
+                using HttpResponseMessage response = httpClient.GetAsync(mod.ModId).Result;
+
+                var htmlResponse = response.Content.ReadAsStringAsync().Result;
+                int modifiedIndex = htmlResponse.IndexOf("Last Modified</dt>");
+                string modifiedDate = htmlResponse.Substring(modifiedIndex + 54, 10);
+                Console.WriteLine($"{mod.Name},{modifiedDate}");
+            }
+            
+
         }
     }
 }
